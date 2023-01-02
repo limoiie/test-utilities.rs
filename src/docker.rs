88de430c3fd43aeb40724ecfb9c9a5758da1eac0
limoiie <limo.iie4@gmail.com>
2023-01-02 20:19:32 +0800

    refactor: Add new api for binding volume

--- src/docker.rs
+++ src/docker.rs
@@ -1,18 +1,18 @@
 use std::collections::HashMap;
 
-use bollard::models::{ContainerInspectResponse, PortBinding, PortMap};
+use bollard::models::{ContainerInspectResponse, PortBinding};
 use bollard::{
     container::{CreateContainerOptions, StartContainerOptions},
     service::HostConfig,
 };
 
 pub struct ContainerHandle {
-    container_id: String,
+    pub container_id: String,
     pub name: Option<String>,
+    pub host_ip: String,
+    pub default_host_port: Option<String>,
+    pub protocol: Option<String>,
     docker: bollard::Docker,
-    host_ip: String,
-    default_host_port: Option<String>,
-    protocol: Option<String>,
 }
 
 impl ContainerHandle {
@@ -55,64 +55,71 @@ impl Drop for ContainerHandle {
 
 #[derive(Default)]
 pub struct Builder {
-    /// Image
-    image: String,
-    /// Port mapping table
-    port_mapping: Option<PortMap>,
+    /// Container config
+    config: bollard::container::Config<String>,
+    /// Container create options
+    create_options: Option<CreateContainerOptions<String>>,
     /// Default accessing protocol
     protocol: Option<String>,
     /// Default accessing port
     default_port: Option<String>,
-    /// Container name
-    name: Option<String>,
 }
 
 impl Builder {
-    pub fn new(image: &str) -> Self {
+    pub fn new<S: Into<String>>(image: S) -> Self {
+        let image = image.into();
+        let protocol = match image.as_str() {
+            "mongo" => Some("mongodb".to_owned()),
+            "redis" => Some("redis".to_owned()),
+            _ => None,
+        };
         Builder {
-            image: image.to_string(),
-            port_mapping: None,
-            protocol: None,
+            config: bollard::container::Config {
+                image: Some(image),
+                host_config: Some(HostConfig {
+                    auto_remove: Some(true),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            create_options: None,
+            protocol,
             default_port: None,
-            name: None,
         }
     }
 
     pub fn bind_port<S, T>(mut self, host_port: Option<S>, port: T) -> Self
     where
-        S: AsRef<str>,
-        T: AsRef<str>,
+        S: Into<String>,
+        T: Into<String>,
     {
-        let port = canonicalize_port(port.as_ref().to_string());
+        let port = canonicalize_port(port.into());
         let host_ip = "localhost".to_string();
-        let host_port = host_port
-            .map(|s| s.as_ref().to_string())
-            .unwrap_or(port.clone());
+        let host_port = host_port.map(Into::into).unwrap_or(port.clone());
         let binding = PortBinding {
             host_ip: Some(host_ip),
             host_port: Some(host_port),
         };
 
-        if self.port_mapping.is_none() {
-            self.port_mapping = Some(HashMap::new());
+        let host_config = self.host_config();
+        if host_config.port_bindings.is_none() {
+            host_config.port_bindings = Some(HashMap::new());
         }
-
-        let port_mapping = self.port_mapping.as_mut().unwrap();
-        if let Some(Some(bindings)) = port_mapping.get_mut(port.as_str()) {
+        let port_bindings = host_config.port_bindings.as_mut().unwrap();
+        if let Some(Some(bindings)) = port_bindings.get_mut(port.as_str()) {
             bindings.push(binding)
         } else {
-            port_mapping.insert(port, Some(vec![binding]));
+            port_bindings.insert(port, Some(vec![binding]));
         }
-
         self
     }
 
     pub fn bind_port_as_default<S, T>(mut self, host_port: Option<S>, port: T) -> Self
     where
-        S: AsRef<str>,
-        T: AsRef<str>,
+        S: Into<String>,
+        T: Into<String>,
     {
-        let port = canonicalize_port(port.as_ref().to_string());
+        let port = canonicalize_port(port.into());
         self.default_port = Some(port.clone());
         self.bind_port(host_port, port.as_str())
     }
@@ -124,67 +131,70 @@ impl Builder {
         self.bind_port(Some(host_port), port)
     }
 
-    pub fn name<S: AsRef<str>>(mut self, name: S) -> Self {
-        self.name = Some(name.as_ref().to_string());
+    pub fn bind_volume<S: Into<String>>(mut self, bind: S) -> Self {
+        let host_config = self.host_config();
+        if host_config.binds.is_none() {
+            host_config.binds = Some(Vec::new());
+        }
+        host_config.binds.as_mut().unwrap().push(bind.into());
         self
     }
 
-    pub fn protocol<S: AsRef<str>>(mut self, protocol: S) -> Self {
-        self.protocol = Some(protocol.as_ref().to_string());
+    pub fn name<S: Into<String>>(mut self, name: S) -> Self {
+        self.create_options().name = name.into();
         self
+    }
+
+    pub fn protocol<S: Into<String>>(mut self, protocol: S) -> Self {
+        self.protocol = Some(protocol.into());
+        self
+    }
+
+    pub fn host_config(&mut self) -> &mut HostConfig {
+        self.config.host_config.as_mut().unwrap()
+    }
+
+    fn create_options(&mut self) -> &mut CreateContainerOptions<String> {
+        if self.create_options.is_none() {
+            self.create_options = Some(Default::default())
+        }
+        self.create_options.as_mut().unwrap()
     }
 
     pub async fn build_disposable(self) -> ContainerHandle {
         let host_ip = "localhost".to_string();
-        let protocol = self.protocol.or_else(|| match self.image.as_str() {
-            "mongo" => Some("mongodb".to_owned()),
-            "redis" => Some("redis".to_owned()),
-            _ => None,
-        });
-
         // should be consistent with host_ip
         let docker = bollard::Docker::connect_with_local_defaults().unwrap();
-        let options = self
-            .name
-            .as_ref()
-            .map(|name| CreateContainerOptions { name });
-        let host_config = HostConfig {
-            auto_remove: Some(true),
-            port_bindings: self.port_mapping,
-            ..Default::default()
-        };
-        let config = bollard::container::Config {
-            image: Some(self.image.as_str()),
-            host_config: Some(host_config),
-            ..Default::default()
-        };
-        let container_handle = docker.create_container(options, config).await.unwrap();
+        let container_handle = docker
+            .create_container(self.create_options, self.config)
+            .await
+            .unwrap();
         docker
             .start_container(&container_handle.id, None::<StartContainerOptions<String>>)
             .await
             .unwrap();
-
-        let info = docker
+        let container_info = docker
             .inspect_container(&container_handle.id, None)
             .await
             .unwrap();
-        let default_host_port = self.default_port.map(|port| {
-            info.get_host_port(Some(host_ip.as_str()), port.as_str())
-                .unwrap()
-        });
+
+        let default_host_port = self
+            .default_port
+            .and_then(|port| container_info.get_host_port(Some(host_ip.as_str()), port.as_str()));
 
         ContainerHandle {
             container_id: container_handle.id,
-            name: self.name,
-            docker,
+            name: container_info.get_name(),
             host_ip,
-            protocol,
+            protocol: self.protocol,
             default_host_port,
+            docker,
         }
     }
 }
 
-fn canonicalize_port(port: String) -> String {
+fn canonicalize_port<S: Into<String>>(port: S) -> String {
+    let port = port.into();
     if port.contains('/') {
         port
     } else {
@@ -194,6 +204,7 @@ fn canonicalize_port(port: String) -> String {
 
 trait ContainerInspectResponseExt {
     fn get_host_port<S: AsRef<str>>(&self, host_ip: Option<S>, port: S) -> Option<String>;
+    fn get_name(&self) -> Option<String>;
 }
 
 impl ContainerInspectResponseExt for ContainerInspectResponse {
@@ -228,6 +239,13 @@ impl ContainerInspectResponseExt for ContainerInspectResponse {
         }
         None
     }
+
+    fn get_name(&self) -> Option<String> {
+        self.name.as_ref().map(|name| match name.strip_prefix("/") {
+            Some(stripped) => stripped.to_string(),
+            None => name.clone(),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -239,7 +257,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_build_docker_handle() {
-        let host_ip = "localhost".to_string();
+        let host_ip = "localhost";
         let docker = bollard::Docker::connect_with_local_defaults().unwrap();
         let name: String = fake::faker::lorem::en::Word().fake();
         let host_port = "28017";
@@ -257,7 +275,7 @@ mod tests {
             assert!(info_opt.is_ok());
 
             let info = info_opt.unwrap();
-            let expected_host_port = info.get_host_port(Some(host_ip.as_str()), port);
+            let expected_host_port = info.get_host_port(Some(host_ip), port);
             let expected_url = format!("mongodb://localhost:{host_port}/");
 
             assert_eq!(host_port, expected_host_port.as_ref().unwrap().as_str());
@@ -275,7 +293,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_build_docker_handle_with_auto_port() {
-        let host_ip = "localhost".to_string();
+        let host_ip = "localhost";
         let docker = bollard::Docker::connect_with_local_defaults().unwrap();
         let name: String = fake::faker::lorem::en::Word().fake();
         let host_port = "0";
@@ -294,7 +312,7 @@ mod tests {
             assert!(info_opt.is_ok());
 
             let info = info_opt.unwrap();
-            let expected_host_port = info.get_host_port(Some(host_ip.as_str()), port);
+            let expected_host_port = info.get_host_port(Some(host_ip), port);
             let expected_url = format!(
                 "mongodb://localhost:{host}/",
                 host = expected_host_port.as_ref().unwrap()
